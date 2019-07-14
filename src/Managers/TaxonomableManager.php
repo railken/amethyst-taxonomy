5346955063f1805eaa9681268d9b6c5e030e53d1
railken <railken@protonmail.ch>
2019-07-14 13:05:00 +0200

    phpdoc

--- src/Managers/TaxonomableManager.php
+++ src/Managers/TaxonomableManager.php
@@ -5,6 +5,14 @@ namespace Amethyst\Managers;
 use Amethyst\Common\ConfigurableManager;
 use Railken\Lem\Manager;
 
+/**
+ * @method \Amethyst\Models\Taxonomable newEntity()
+ * @method \Amethyst\Schemas\TaxonomableSchema getSchema()
+ * @method \Amethyst\Repositories\TaxonomableRepository getRepository()
+ * @method \Amethyst\Serializers\TaxonomableSerializer getSerializer()
+ * @method \Amethyst\Validators\TaxonomableValidator getValidator()
+ * @method \Amethyst\Authorizers\TaxonomableAuthorizer getAuthorizer()
+ */
 class TaxonomableManager extends Manager
 {
     use ConfigurableManager;
--- src/Managers/TaxonomyManager.php
+++ src/Managers/TaxonomyManager.php
@@ -5,6 +5,14 @@ namespace Amethyst\Managers;
 use Amethyst\Common\ConfigurableManager;
 use Railken\Lem\Manager;
 
+/**
+ * @method \Amethyst\Models\Taxonomy newEntity()
+ * @method \Amethyst\Schemas\TaxonomySchema getSchema()
+ * @method \Amethyst\Repositories\TaxonomyRepository getRepository()
+ * @method \Amethyst\Serializers\TaxonomySerializer getSerializer()
+ * @method \Amethyst\Validators\TaxonomyValidator getValidator()
+ * @method \Amethyst\Authorizers\TaxonomyAuthorizer getAuthorizer()
+ */
 class TaxonomyManager extends Manager
 {
     use ConfigurableManager;
